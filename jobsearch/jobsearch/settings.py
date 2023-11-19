48b9fc5b14e7de7645511947ad2fc989cd93abee
Wambong <64046698+Wambong@users.noreply.github.com>
2023-11-19 03:04:03 +0300

    Update settings.py

--- jobsearch/jobsearch/settings.py
+++ jobsearch/jobsearch/settings.py
@@ -5,7 +5,7 @@ from decouple import config
 
 
 
-# BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -70,14 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'jobsearch.wsgi.application'
 
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 
 DATABASES = {
